f88aebb7220a804b36493d2e267169bd45dce002
v3coh <125557438+v3coh@users.noreply.github.com>
2023-07-09 14:54:02 +0300

    Update contact page and add footer page

---
+++ src/Componets/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "../Style/style.css"
+export default function Footer() {
+  return (
+    <div class="row-last">
+            <div class="row row-cols-1 row-cols-md-3  p-3 text-white">
+                <div class="col">
+                    <h4>CALL US</h4>
+                    <p>+9647713402589</p>
+                </div>
+                <div class="col">
+                    <h4>LOCATION</h4>
+                    <p>Iraq-Baghdad</p>
+                </div>
+                <div class="col">
+                    <h4>Email</h4>
+                    <p>hudaadnan399@gmail.com</p>
+                </div>
+            </div>
+        </div>
+  )
+}
